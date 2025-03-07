b396974bfcadf556dac92da7518a57538fdbc035
Rafael Shroom <68326435+shroom-r@users.noreply.github.com>
2025-03-07 11:04:37 +0100

    Added flask-migration to project and configure project to work with it

--- flaskr/app/app.py
+++ flaskr/app/app.py
@@ -1,27 +1,54 @@
 from flask import Flask
 from markupsafe import escape
-from flask import url_for
 from flask import render_template
 from database.db import db
 import os
 # Import models to create tables in database
-from models.User import User
-from models.Event import Event
-from models.EventPost import EventPost
+from config import TestingConfig, ProductionConfig, DevelopmentConfig
+from flask_migrate import Migrate
+
+def load_models():
+    from models.User import User
+    from models.Event import Event
+    from models.EventPost import EventPost
 
 # Define applications
 app = Flask(__name__)
 
-# Configure connection to database
-with open(os.environ.get("DATABASE_PASSWORD_FILE"), "r") as f:
-    databasePassword = f.read()
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://root:{databasePassword}@db/example"
-    
 
+env = 'testing'
+try:
+    env = os.environ['FLASK_ENV']
+except KeyError:
+    pass
+
+if env == 'development':
+    config = DevelopmentConfig()
+elif env=='production':
+    config = ProductionConfig()
+else:
+    config = TestingConfig()
+
+
+# Configure connection to database
+# with open(os.environ.get("DATABASE_PASSWORD_FILE"), "r") as f:
+#     databasePassword = f.read()
+#     app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://root:{databasePassword}@db/plat_nner_db"
+app.config.from_object(config)
+
+# db = SQLAlchemy(app)
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+
+
+# with app.app_context():
+#     db.create_all()
+
+load_models()
+
+migrate = Migrate(app, db)
+
+
 
 @app.route("/")
 def index():
---
+++ flaskr/app/config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config(object):
+    TESTING = False
+
+class ProductionConfig(Config):
+    try:
+        with open(os.environ.get("DATABASE_PASSWORD_FILE"), "r") as f:
+            databasePassword = f.read()
+            SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://root:{databasePassword}@db/plat_nner_db"
+    except:
+        pass
+
+class DevelopmentConfig(Config):
+    try:
+        with open(os.environ.get("DATABASE_PASSWORD_FILE"), "r") as f:
+            databasePassword = f.read()
+            SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://root:{databasePassword}@db/plat_nner_db"
+    except:
+        pass
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    TESTING = True
