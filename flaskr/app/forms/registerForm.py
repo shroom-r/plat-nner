53466a9a3af117c170b39d87189a880ff9c089d1
Rafael Shroom <68326435+shroom-r@users.noreply.github.com>
2025-05-11 18:59:24 +0200

    Added minimum password length on registering form

--- flaskr/app/forms/registerForm.py
+++ flaskr/app/forms/registerForm.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class RegisterForm(FlaskForm):
     secretCode = StringField("Secret code", validators=[DataRequired()], render_kw={'autofocus': True})
     username = StringField("Username",validators=[DataRequired()])
-    password = PasswordField("Password",validators=[DataRequired()])
+    password = PasswordField("Password",validators=[DataRequired(), Length(min=6)])
     submit = SubmitField("Register")
     message = ""
