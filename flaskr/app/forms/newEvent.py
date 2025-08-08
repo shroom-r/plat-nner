ac60fb6cadefa722ef8f203edfcd6ea91fbf477a
Rafael Shroom <68326435+shroom-r@users.noreply.github.com>
2025-08-08 09:14:44 +0200

    Added default date on event creation

--- flaskr/app/forms/newEvent.py
+++ flaskr/app/forms/newEvent.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateField
 from wtforms.validators import DataRequired, Optional
+import datetime
 
 class NewEvent(FlaskForm):
-    date = DateField("Date", validators=[DataRequired()], format='%Y-%m-%d')
+    date = DateField("Date", validators=[DataRequired()], format='%Y-%m-%d', default=datetime.date.today)
     name = StringField("Nom de l'évènement", validators=[DataRequired()])
     submit = SubmitField("Créer")
